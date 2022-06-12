adf50afa58ac04ae450c1ec07682875a47611b52
Rob Sokolowski <rpsoko@gmail.com>
2022-06-12 10:37:17 -0400

    bigger data, change response format

--- api/server.py
+++ api/server.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import time
 import typing as t
+
 
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.models import Response
+
 from api.counter import StatefulCounter
 from api.duck_wrapper import DuckCore
-from api.types import PeakResponse, IncrementResponse, DuckDbQueryRequest
+from api.types import PeakResponse, IncrementResponse, DuckDbQueryRequest, DuckDbQueryResponse
+from fastapi.responses import JSONResponse
 
 counter = StatefulCounter()
 duck_core = DuckCore()
@@ -31,18 +36,26 @@ def counter_router() -> APIRouter:
     return router
 
 
-
-
 def duckdb_router() -> APIRouter:
     router = APIRouter()
 
     @router.post("/duckdb")
-    async def execute_query(req: DuckDbQueryRequest) -> str:
+    async def execute_query(req: DuckDbQueryRequest) -> DuckDbQueryResponse:
+        tic = time.perf_counter()
         df: pd.DataFrame = duck_core.execute_as_df(query_str=req.query_str)
-        return df.to_json(orient="columns")
+        toc = time.perf_counter()
+        print(f"{toc-tic} secs")
 
+        return DuckDbQueryResponse(
+            data=df.to_dict(orient="list")
+        )
     return router
 
+
+def import_csv_files() -> None:
+    """The following are loaded into DuckDB's memory"""
+    duck_core.import_csv_file(path="./data/president_polls.csv", table_name="president_polls", )
+    duck_core.import_csv_file(path="./data/president_polls_historical.csv", table_name="president_polls_historical", )
 
 
 def get_app_instance() -> FastAPI:
@@ -59,9 +72,7 @@ def get_app_instance() -> FastAPI:
     app.include_router(counter_router())
     app.include_router(duckdb_router())
 
-    duck_core.import_csv_file(path="./tests/data/president_polls.csv", table_name="president_polls")
-
     return app
 
-
+import_csv_files()
 app = get_app_instance()
