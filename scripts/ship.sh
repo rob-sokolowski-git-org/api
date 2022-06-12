3b6ae10eb6b025bea54d1af88fbecb2cce0da593
Rob Sokolowski <rpsoko@gmail.com>
2022-06-12 11:48:37 -0400

    bump memory

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -10,5 +10,7 @@ gcloud run deploy robsoko-api \
     --image gcr.io/fir-sandbox-326008/robsoko-api:latest \
     --platform managed \
     --region us-east4 \
+    --memory 1024Mi \
+    --cpu 2 \
     --min-instances 0 \
     --max-instances 1
