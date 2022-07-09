1889a653af704f859404fb140093c71c47374f54
Rob Sokolowski <rpsoko@gmail.com>
2022-07-08 23:29:08 -0400

    2 tests

--- api/object_storage.py
+++ api/object_storage.py
@@ -2,8 +2,12 @@ from google.cloud import storage
 
 import os
 
-# Just playing with an idea of not using classes, and using modules instead.. why not?
+# Just playing with an idea of not using classes, and using modules instead.. eh, why not?
 _CLIENT: storage.Client = storage.Client()
+
+def create_bucket(bucket_name: str):
+    _CLIENT.create_bucket(bucket_or_name=bucket_name)
+
 
 def send_file(path: str, bucket_name: str, key: str):
     bucket = _CLIENT.bucket(bucket_name=bucket_name)
--- tests/object_storage_test.py
+++ tests/object_storage_test.py
@@ -4,10 +4,15 @@ from api import object_storage as obj
 from google.cloud import storage
 
 
-def test_stuff():
+
+def test_create_bucket():
+    bucket_name="bucket-creation-test"
+    obj.create_bucket(bucket_name=bucket_name)
+
+
+def test_upload_file():
     test_file = "./data/president_polls.csv"
     bucket_name = "local-dev-tests"
     key = "test-blob"
-
 
     obj.send_file(path=test_file, bucket_name=bucket_name, key=key)
