8cde90beb0ef1f3c520d0363e4adf95102d69a3a
Rob Sokolowski <rpsoko@gmail.com>
2022-07-12 10:09:24 -0400

    verify things work

--- tests/api_tests/http_api_test.py
+++ tests/api_tests/http_api_test.py
@@ -10,8 +10,8 @@ TEST_TEMP_DIR = "./tests/temp"
 
 # TODO: When I do the CloudBuild work these should be set via env files
 # TARGET_HOST = "http://localhost:8000"  # local dev
-# TARGET_HOST = "http://host.docker.internal:8080"  # local gunicorn
-TARGET_HOST = "https://api.robsoko.tech"  # production
+TARGET_HOST = "http://host.docker.internal:8080"  # local gunicorn
+# TARGET_HOST = "https://api.robsoko.tech"  # production
 
 
 @pytest.fixture(scope="module")
